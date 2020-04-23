ad9eecf37adf4461e2fb76948a20fe44c1b88d97
gitonga24 <39718444+gitonga24@users.noreply.github.com>
2020-04-22 21:07:02 -0400

    initial save

---
+++ src/main/java/com/gitonga/plutoAir/PlutoAirApplication.java
@@ -0,0 +1,13 @@
+package com.gitonga.plutoAir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlutoAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlutoAirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitonga/plutoAir/PlutoAirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitonga.plutoAir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutoAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
